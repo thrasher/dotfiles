b92ec55f1e51c5ee13b248c38619b706dfc27bfc
Jason Thrasher <jasonthrasher@gmail.com>
2024-09-06 08:36:26 +0200

    added alternative way of doing pull, but disabled

--- bin/gitpull.sh
+++ bin/gitpull.sh
@@ -1,4 +1,4 @@
-#/bin/bash
+#!/usr/bin/env bash
 #
 # Pull an update for a bunch of git repos in my dev env
 # assume ln -s Documents/dev dev on OSX
@@ -12,3 +12,11 @@ for path in `pwd`/*; do
     #continue
     cd $path  && git pull
 done
+
+### Another way of doing this, may be more reliable
+
+# find . -type d -depth 1
+#set REPOS = `find repos -type d -depth 1`
+#echo $REPOS
+#find . -type d -depth 1 -exec git --git-dir={}/.git --work-tree=$PWD/{} pull origin master \;
+#find . -type d -depth 1 -exec git --git-dir={}/.git --work-tree=$PWD/{} pull \;
